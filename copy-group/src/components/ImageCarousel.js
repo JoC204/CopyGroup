e416bce9523828f1f935b6f2cfc3451fc019706e
JoC204 <jbarrionuevo204@gmail.com>
2024-07-20 11:16:35 -0300

    Se agregan imagenes a carrusel de imagenes en Home.js

--- copy-group/src/components/ImageCarousel.js
+++ copy-group/src/components/ImageCarousel.js
@@ -11,10 +11,10 @@ import image3 from '../img/3.jpg';
 import image4 from '../img/4.jpg';
 import image5 from '../img/5.jpg';
 import image6 from '../img/6.jpg';
-import image7 from '../img/calco-uno.jpg';
-import image8 from '../img/calco-dos.jpg';
-import image9 from '../img/ploteo-uno.jpg';
-import image10 from '../img/impresion-uno.jpg';
+import image7 from '../img/7.jpg';
+import image8 from '../img/8.jpg';
+import image9 from '../img/10.jpg';
+import image10 from '../img/11.jpg';
 
 const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10];
 
