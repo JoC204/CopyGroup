7259b4dc20fb72dfe0e02a3276295e5283f18979
JoC204 <jbarrionuevo204@gmail.com>
2024-07-28 23:16:01 -0300

    Se cambiaron las direcciones de imagenes a las definitivas.

--- copy-group/src/components/DirImg/ImgSerigrafia.js
+++ copy-group/src/components/DirImg/ImgSerigrafia.js
@@ -1,12 +1,12 @@
 // src/components/ImgPlots.js
-import image1 from "../../img/impresion-uno.jpg";
-import image2 from "../../img/impresion-dos.jpg";
-import image3 from "../../img/calco-dos.jpg";
-import image4 from "../../img/calco-uno.jpg";
-import image5 from "../../img/ploteo-dos.jpg";
-import image6 from "../../img/ploteo-uno.jpg";
-import image7 from "../../img/impresion-dos.jpg";
-import image8 from "../../img/impresion-dos.jpg";
+import image1 from "../../img/Serigrafia/001.jpg";
+import image2 from "../../img/Serigrafia/002.jpg";
+import image3 from "../../img/Serigrafia/003.jpg";
+import image4 from "../../img/Serigrafia/004.jpg";
+import image5 from "../../img/Serigrafia/005.jpg";
+import image6 from "../../img/Serigrafia/006.jpg";
+import image7 from "../../img/Serigrafia/007.jpg";
+import image8 from "../../img/Serigrafia/008.jpg";
 
 const ImgSerigrafia = [
   image1,
